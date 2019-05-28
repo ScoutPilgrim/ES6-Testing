7fd5ee82eff1912577c556012d2754c0ce9ff5a7
ScoutPilgrim <ianchristner@gmail.com>
2019-05-28 15:46:48 -0700

    Updated tests. All so far passing

--- spec/ageCalc-spec.js
+++ spec/ageCalc-spec.js
@@ -3,7 +3,27 @@ import { FormAge } from './../src/ageCalc.js';
 describe('FormAge', function(){
 
   it('Correctly gets the current Year', function(){
-    var testFormAgeObj = new FormAge(24);
+    var testFormAgeObj = new FormAge(1995);
     expect(testFormAgeObj.currYear === 2019);
+  });
+  it('Correctly finds the users age based on their entered birth year', function(){
+    var testFormAgeObj = new FormAge(1995);
+    expect(testFormAgeObj.age === 24);
+  });
+  it('Correctly converts years into Mercury Years', function(){
+    var testFormAgeObj = new FormAge(1995);
+    expect(testFormAgeObj.mercuryYears === 5.76);
+  });
+  it('Correctly converts into Venus Years', function(){
+    var testFormAgeObj = new FormAge(1995);
+    expect(testFormAgeObj.venusYears === 14.88);
+  });
+  it('Correctly converts into Mars Years', function(){
+    var testFormAgeObj = new FormAge(1995);
+    expect(testFormAgeObj.marsYears === 45.12);
+  });
+  it('Correctly converts into Jupiter Years', function(){
+    var testFormAgeObj = new FormAge(1995);
+    expect(testFormAgeObj.jupiterYears === 284.64);
   });
 });
