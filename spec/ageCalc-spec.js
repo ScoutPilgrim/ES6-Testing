869b540479d130f34650ef364365ef073837281f
ScoutPilgrim <ianchristner@gmail.com>
2019-05-29 09:47:52 -0700

    Added more functionality to FormAge class. Tested added func and everyhting is in the green

--- spec/ageCalc-spec.js
+++ spec/ageCalc-spec.js
@@ -1,29 +1,90 @@
 import { FormAge } from './../src/ageCalc.js';
 
+//Const Variables. The Years used for testing.
+const testYear = 1995;
+const testYear_Old = 1920;
+
+//Tests
 describe('FormAge', function(){
 
   it('Correctly gets the current Year', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.currYear === 2019);
   });
   it('Correctly finds the users age based on their entered birth year', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.age === 24);
   });
   it('Correctly converts years into Mercury Years', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.mercuryYears === 5.76);
   });
   it('Correctly converts into Venus Years', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.venusYears === 14.88);
   });
   it('Correctly converts into Mars Years', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.marsYears === 45.12);
   });
   it('Correctly converts into Jupiter Years', function(){
-    var testFormAgeObj = new FormAge(1995);
+    var testFormAgeObj = new FormAge(testYear);
     expect(testFormAgeObj.jupiterYears === 284.64);
+  });
+  it('Correctly generates Life Expectancy for mercury', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.mercuryLifeExpectancy === 19.2);
+  });
+  it('Correctly generates Life Expectancy for venus', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.venusLifeExpectancy === 49.6);
+  });
+  it('Correctly generates Life Expectancy for mars', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.marsLifeExpectancy === 150.4);
+  });
+  it('Correctly generates Life Expectancy for jupiter', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.jupiterLifeExpectancy === 948.8);
+  });
+  it('Correctly generates a 0 if too young for earth Life Expectancy', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.earthExceed === 0);
+  });
+  it('Correctly generates life over Expectancy for earth if old enough', function(){
+    var testFormAgeObj = new FormAge(testYear_Old);
+    expect(testFormAgeObj.earthExceed === 19);
+  });
+  it('Correctly generates a 0 if too young for mercury Life Expectancy', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.mercuryExceed === 0);
+  });
+  it('Correctly generates life over Expectancy for mercury if old enough', function(){
+    var testFormAgeObj = new FormAge(testYear_Old);
+    expect(testFormAgeObj.mercuryExceed === 4.56);
+  });
+  it('Correctly generates a 0 if too young for venus Life Expectancy', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.venusExceed === 0);
+  });
+  it('Correctly generates life over Expectancy for venus if old enough', function(){
+    var testFormAgeObj = new FormAge(testYear_Old);
+    expect(testFormAgeObj.venusExceed === 11.78);
+  });
+  it('Correctly generates a 0 if too young for mars Life Expectancy', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.marsExceed === 0);
+  });
+  it('Correctly generates life over Expectancy for mars if old enough', function(){
+    var testFormAgeObj = new FormAge(testYear_Old);
+    expect(testFormAgeObj.marsExceed === 35.72);
+  });
+  it('Correctly generates a 0 if too young for jupiter Life Expectancy', function(){
+    var testFormAgeObj = new FormAge(testYear);
+    expect(testFormAgeObj.jupiterExceed === 0);
+  });
+  it('Correctly generates life over Expectancy for jupiter if old enough', function(){
+    var testFormAgeObj = new FormAge(testYear_Old);
+    expect(testFormAgeObj.jupiterExceed === 225.34);
   });
 });
--- src/ageCalc.js
+++ src/ageCalc.js
@@ -2,36 +2,32 @@ export class FormAge
 {
   constructor(birthYear)
   {
-    this.secondsPerYear = 31557600;
     this.mercuryConvert = .24;
     this.venusConvert = .62;
     this.marsConvert = 1.88;
     this.jupiterConvert = 11.86;
-    this.lifeExpectancy = 80;
+    this.earthLifeExpectancy = 80;
     this.currYear = new Date().getYear();
     this.age = this.YearDifference(birthYear);
-    this.yearSeconds = this.YearDifferenceInSeconds(birthYear);
-    this.ageSeconds = this.AgeToSeconds();
     this.mercuryYears = this.AgeToMercury();
     this.venusYears = this.AgeToVenus();
     this.marsYears = this.AgeToMars();
     this.jupiterYears = this.AgeToJupiter();
+    this.mercuryLifeExpectancy = this.GenerateMercuryLife();
+    this.venusLifeExpectancy = this.GenerateVenusLife();
+    this.marsLifeExpectancy = this.GenerateMarsLife();
+    this.jupiterLifeExpectancy = this.GenerateJupiterLife();
+    this.earthExceed = this.ExceedLife("EAR");
+    this.mercuryExceed = this.ExceedLife("MER");
+    this.venusExceed = this.ExceedLife("VEN");
+    this.marsExceed = this.ExceedLife("MAR");
+    this.jupiterExceed = this.ExceedLife("JUP");
   }
-}
-
-FormAge.prototype.AgeToSeconds = function AgeToSeconds(){
-  var ageToSeconds = this.age * this.secondsPerYear;
-  return ageToSeconds;
 }
 
 FormAge.prototype.YearDifference = function YearDifference(startDate){
   var differenceToAge = this.currYear - startDate;
   return differenceToAge;
-}
-
-FormAge.prototype.YearDifferenceInSeconds = function YearDifferenceInSeconds(startDate){
-  var yearDifferenceInSeconds = (this.currYear - startDate) * this.secondsPerYear;
-  return yearDifferenceInSeconds;
 }
 
 FormAge.prototype.AgeToMercury = function AgeToMercury(){
@@ -52,4 +48,55 @@ FormAge.prototype.AgeToMars = function AgeToMars(){
 FormAge.prototype.AgeToJupiter = function AgeToJupiter(){
   var localJupiter = this.age * this.jupiterConvert;
   return localJupiter;
+}
+
+FormAge.prototype.GenerateMercuryLife = function GenerateMercuryLife(){
+  var merLife = this.earthLifeExpectancy * this.mercuryConvert;
+  return merLife;
+}
+
+FormAge.prototype.GenerateVenusLife = function GenerateVenusLife(){
+  var venLife = this.earthLifeExpectancy * this.venusConvert;
+  return venLife;
+}
+
+FormAge.prototype.GenerateMarsLife = function GenerateMarsLife(){
+  var marsLife = this.earthLifeExpectancy * this.marsConvert;
+  return marsLife;
+}
+
+FormAge.prototype.GenerateJupiterLife = function GenerateJupiterLife(){
+  var jupiterLife = this.earthLifeExpectancy * this.jupiterConvert;
+  return jupiterLife;
+}
+
+FormAge.prototype.ExceedLife = function ExceedLife(myCase){
+  switch(myCase){
+    case "EAR":
+      if(this.age > this.earthLifeExpectancy){
+        return this.age - this.earthLifeExpectancy;
+      }
+      return 0;
+    case "MER":
+      if(this.mercuryYears > this.mercuryLifeExpectancy){
+        return this.mercuryYears - this.mercuryLifeExpectancy;
+      }
+      return 0;
+    case "VEN":
+      if(this.venusYears > this.venusLifeExpectancy){
+        return this.venusYears - this.venusLifeExpectancy;
+      }
+      return 0;
+    case "MAR":
+      if(this.marsYears > this.marsLifeExpectancy){
+        return this.marsYears - this.marsLifeExpectancy;
+      }
+      return 0;
+    case "JUP":
+      if(this.jupiterYears > this.jupiterLifeExpectancy){
+        return this.jupiterYears - this.jupiterLifeExpectancy;
+      }
+      return 0;
+  }
+  return 0;
 }
