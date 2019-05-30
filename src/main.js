e7e8c409325ba3f0017e2c939b8a550997536a18
ScoutPilgrim <ianchristner@gmail.com>
2019-05-30 09:59:54 -0700

    Updated Index.html and main.js to properly reflect all properties of the FormAge class

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ let mercuryText = "Your age on Mercury: ";
 let venusText = "Your age on Venus ";
 let marsText = "Your age on Mars: ";
 let jupiterText = "Your age on Jupiter: ";
+const earthOver = "You are older than earth's life expectancy by ";
+const mercuryOver = "You are older than mercury's life expectancy by ";
+const venusOver = "You are older than venus's life expectancy by ";
+const marsOver = "You are older than mars's life expectancy by ";
+const jupiterOver = "You are older than jupiter's life expectancy by ";
 
 $(document).ready(function(){
   $("#myForm").submit(function(event){
@@ -20,5 +25,16 @@ $(document).ready(function(){
     $("#venusAge").text(venusText + formBuildObj.venusYears);
     $("#marsAge").text(marsText + formBuildObj.marsYears);
     $("#jupiterAge").text(jupiterText + formBuildObj.jupiterYears);
+    checkIfOver(formBuildObj);
   });
+  //Helper functions
+  function checkIfOver(myObj){
+    if(myObj.age > myObj.earthLifeExpectancy){
+      $("#earthOver").text(earthOver + myObj.earthExceed);
+      $("#mercuryOver").text(mercuryOver + myObj.mercuryExceed);
+      $("#venusOver").text(venusOver + myObj.venusExceed);
+      $("#marsOver").text(marsOver + myObj.marsExceed);
+      $("#jupiterOver").text(jupiterOver + myObj.jupiterExceed);
+    }
+  }
 });
